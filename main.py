901fc95bb88eca913d9fdf957c42df3fe6ad125c
KiSARETO77 <106799027+KiSARETO77@users.noreply.github.com>
2022-06-08 23:39:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-import os
-os.system("python main1.py")
-os.system("python main2.py")
+import os,threding
+def F():
+	os.system("python main2.py")
+
+def G():
+	os.system("python main1.py")
+	
+S=threding.Thread(traget=F,args=())
+D=threding.Thread(traget=G,args=())
+
+S.start()
+D.start()
